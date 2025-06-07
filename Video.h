b638370baa17201947946937abbd346823ced131
jpgav <jpgavidiaa@gmail.com>
2025-06-06 18:21:58 -0600

    Se arregló video.h

--- Video.h
+++ Video.h
@@ -7,25 +7,24 @@ using namespace std;
 
 class Video {
 private:
-    int id, duracion;
+    int id;
     string titulo, genero;
     float calificacion;
 
 public:
     Video();
-    Video(int _id, int _duracion, string _titulo, string _genero, float _calificacion);
+    Video(int _id, string _titulo, string _genero, float _calificacion);
     virtual ~Video();
     int get_id();
-    int get_duracion();
     string get_titulo();
     string get_genero();
     virtual float get_calificacion();
     void set_id(int _id);
-    void set_duracion(int _duracion);
     void set_titulo(string _titulo);
     void set_genero(string _genero);
     virtual void set_calificacion(float _calificacion);
     virtual void mostrar_info() = 0;
+    virtual bool Peli(){return false;}
 };
 
 #endif // VIDEO_H
