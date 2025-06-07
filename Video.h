eeeeae9161db39d3d8691a8de485d10c2e048d69
jpgav <jpgavidiaa@gmail.com>
2025-06-06 18:34:59 -0600

    Se comentó Video.h

--- Video.h
+++ Video.h
@@ -5,26 +5,40 @@
 #include <iostream>
 using namespace std;
 
-class Video {
+class Video { // Clase base ABSTRACTA PURA
 private:
+    // Atributos de clase base "Video", de la que heredan pelicula y serie
     int id;
     string titulo, genero;
     float calificacion;
 
 public:
+    // Constructores
     Video();
     Video(int _id, string _titulo, string _genero, float _calificacion);
+
+    // Destructor virtual
     virtual ~Video();
+
+    // Getters
     int get_id();
     string get_titulo();
     string get_genero();
-    virtual float get_calificacion();
+
+    // Setters
     void set_id(int _id);
     void set_titulo(string _titulo);
     void set_genero(string _genero);
+
+    // Metodos virtuales que las derivadas pueden sobreescribir
+    virtual float get_calificacion();
     virtual void set_calificacion(float _calificacion);
-    virtual void mostrar_info() = 0;
     virtual bool Peli(){return false;}
+
+    // Method virtual puro, no se implementa en clase base
+    // Obliga a las derivadas a implementarlo
+    virtual void mostrar_info() = 0;
+
 };
 
 #endif // VIDEO_H
